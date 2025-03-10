ba63374acb574b16725acff6ce6e22d4ff2c1392
realprivatescript <realprivatescript@gmail.com>
2025-03-10 19:05:30 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
+const memoryUsage = process.memoryUsage();
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => {
+    console.log("Memory Usage:");
+    console.log(`RSS (Resident Set Size): ${memoryUsage.rss / 1024 / 1024} MB`);
+    console.log(`Heap Total: ${memoryUsage.heapTotal / 1024 / 1024} MB`);
+    console.log(`Heap Used: ${memoryUsage.heapUsed / 1024 / 1024} MB`);
+    console.log(`External: ${memoryUsage.external / 1024 / 1024} MB`);
+    return "Hello Elysia";
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
